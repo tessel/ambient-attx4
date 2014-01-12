d56c88b9d4f45264cda6953c738d004ba1b6cac5
johnnyman727 <jonathan.mckay@students.olin.edu>
2014-01-12 00:21:20 -0500

    Can receive and in the process of setting up duration tracking

--- firmware/src/ambient.c
+++ firmware/src/ambient.c
@@ -9,128 +9,169 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-#define IR_TX 0
-#define MIC 1
-#define CS	2
-#define AMBIENT_LIGHT 7
+// Port A, pin 1
+#define LOUDNESS_PIN 1
 
-#define BUF_SIZE 25
-volatile char LIGHT_BUF[BUF_SIZE];
-volatile char MIC_BUF[BUF_SIZE];
-volatile int LIGHT_BUF_LOC=0;
-volatile int MIC_BUF_LOC=0;
+// Port B, pin 2
+#define CS_PIN  2
+
+// Port A, pin 7
+#define PHOTODIODE_PIN 3
+
+// Port A, pin 2
+#define MIC_PIN 2 
+
+// Port B Pin 1
+#define TRIG_INT 1
+
+// Available commands
+#define ACK_CMD  0
+#define LIGHT_CMD 1
+#define SOUND_CMD  2
+#define LOUDNESS_CMD 3
+#define LIGHT_TRIGGER_CMD 4
+#define LOUDNESS_TRIGGER_CMD 5
+#define TRIGGER_FETCH_CMD 6
+
+// Response constants
+#define ALIVE_CODE 0x55
+#define ACK_CODE 0x33
+#define STOP_CMD 0x16
+
+// Size of buffers (be careful about making this bigger, could run out of bss)
+#define BUF_SIZE 32
+
+// Iterater
 volatile int counter = 0;
 
+// Light trigger to be hit
+volatile char lightTrigger = 0x00;
+// Value read on trigger hit
+volatile char lightTriggerReadVal = 0x00;
+
+// Loudness trigger to be hit
+volatile char loudnessTrigger = 0x00;
+// Value read on trigger hit
+volatile char loudnessTriggerReadVal = 0x00;
+
+
+// The type for the buffer
+typedef struct 
+{
+  volatile char buffer[BUF_SIZE]; 
+  volatile int bufferLocation;
+} DataBuffer;
+
+
+// A buffer and buffer indexer for each source
+volatile DataBuffer LightBuffer;
+DataBuffer MicBuffer;
+DataBuffer LoudnessBuffer;
+
+// Our array of data buffers
+volatile DataBuffer *Buffers[] = {&LightBuffer, &MicBuffer,  &LoudnessBuffer};
+
+void setup(void);
+void setupIO(void);
+void enableSPI(void);
+void setupTimer(void);
 void prepareADC(void);
 int analogRead(char pin);
-void delay(unsigned long ms);
-void delayMicroseconds(unsigned long us);
-void toggleIR(void);
-void initializeBuffers(void);
 
 int main(void) {
 
-    cli();
+    setup();
 
-    // Make the mic pin an input
-    cbi(DDRA, MIC);
+    while (1){};
 
-    // Make the ambient light as input
-    cbi(DDRA, AMBIENT_LIGHT);
+    return 0;
 
-    // Make IR an output for testing
-    sbi(DDRB, IR_TX);
-
-    initializeBuffers();
-
-    // interrupt on INT0 pin falling edge
-    MCUCR = (1<<ISC01);
-
-    // turn on external interrupts!
-    GIMSK |= (1<<INT0);
-
-    // Set CS as OUTPUT
-    cbi(DDRB, CS);
-
-    // Set up pull up
-    sbi(PORTB, CS);
-
-    // Start up slave 
-    spiX_initslave(0);
-
-    // disable spi counter overflow enable
-    USICR&= ~(1<<USIOIE);
-    USICR&= ~(1<<USIWM0);
-
-    // Set up ADC
-    prepareADC();
-
-    // Set clock divider to 8 (1MHz)
-    sbi(TCCR1B, CS11);
-
-    // Set the counter to CTC (clear on match)
-    sbi(TCCR1B, WGM12);
-
-    // Set frequency to 10kHz
-    // Not sure why 47 works better than 50
-    // 8000000/8/50/2 should be 10kHz...
-    OCR1A = 47;
-
-    // Allow interrupts on comp a
-    sbi(TIMSK1, OCIE1A);
-
-    // Unleash the interrupts!
-	sei();
-
-	// Wait for SPI interrupts
-	while(1);
-
-    // We'll never *actually* return. FOOLED YOU COMPILER!
-	return 0;
 }
 
-void initializeBuffers(void) {
+void setup(void) {
+  
+  // Turn off interrupts  
+  cli();
 
-    for (int i = 0; i < BUF_SIZE; i++) {
-        MIC_BUF[i] = 0;
-        LIGHT_BUF[i] = 0;
-    }
+  // Reset buffer locations
+  LightBuffer.bufferLocation = 0;
+  LoudnessBuffer.bufferLocation = 0;
+  MicBuffer.bufferLocation = 0;
+
+  setupIO();
+
+  enableSPI();
+
+  setupTimer();
+
+  sbi(PORTB, TRIG_INT);
+  cbi(PORTB, TRIG_INT);
+
+  // Unleash the interrupts!
+  sei();
 }
 
-ISR(TIM1_COMPA_vect) {
-    // Add light data to buffer
-    analogRead(AMBIENT_LIGHT);
-    // char scaledReading = (char)rawLightReading/4;
-    // LIGHT_BUF[LIGHT_BUF_LOC++] = analogRead(AMBIENT_LIGHT);
+void setupIO(void) {
+  // Make the mic pin an input
+  cbi(DDRA, MIC_PIN);
 
-    // // Add mic data to buffer
-    // MIC_BUF[MIC_BUF_LOC++] = analogRead(MIC);
+  // Make the ambient light as input
+  cbi(DDRA, PHOTODIODE_PIN);
+  
+  // Make the Interrupt pin an output
+  sbi(DDRB, TRIG_INT);
+  
+  // Pull it low
+  cbi(PORTB, TRIG_INT);
+}
 
-    LIGHT_BUF[LIGHT_BUF_LOC++] = 30;
+void enableSPI(void) {
+  // configure: interrupt on INT0 pin falling edge
+  MCUCR = (1<<ISC01);
 
-    if(LIGHT_BUF_LOC > BUF_SIZE){
-        LIGHT_BUF_LOC=0;
-    }
+  // enable interrupt
+  sbi(GIMSK, INT0);
 
-    // Add mic data to buffer
-    MIC_BUF[MIC_BUF_LOC++] = 12;
+  // Set CS as INPUT
+  cbi(DDRB, CS_PIN);
 
-    if(MIC_BUF_LOC > BUF_SIZE){
-        MIC_BUF_LOC=0;
-    }
+  // Set up pull up to keep CS high
+  sbi(PORTB, CS_PIN);
 
-    // toggleIR();
+  // Start up slave
+  spiX_initslave(0);
+
+  // disable spi counter overflow enable
+  USICR&= ~(1<<USIOIE);
+  USICR&= ~(1<<USIWM0);
+}
+
+void setupTimer(void) {
+  // Set clock divider to 8 (1MHz)
+  sbi(TCCR1B, CS11);
+
+  // Set the counter to CTC (clear on match)
+  sbi(TCCR1B, WGM12);
+
+  // Set frequency to 5kHz
+  // (8000000/8/200) Hz
+  OCR1A = 200;
+
+  // Allow interrupts on comp a
+  sbi(TIMSK1, OCIE1A);
 }
 
 void prepareADC(void) {
 
     // Set up ADC clock prescalar to 64 (125kHz) by writing ADPS bits of ADSCRA
-    sbi(ADCSRA, ADPS2);
-    sbi(ADCSRA, ADPS1);
-    cbi(ADCSRA, ADPS0);
+    // sbi(ADCSRA, ADPS2);
+    // sbi(ADCSRA, ADPS1);
+    // cbi(ADCSRA, ADPS0);
 
     // Enable ADC by writing ADEN in ADSCRA
     sbi(ADCSRA, ADEN);
+    // Make sure there are no conversions happening
+    cbi(ADCSRA, ADSC);
 
     // Set reference voltage to AREF by writing to the ADMUX register
     cbi(ADMUX, REFS1);
@@ -151,88 +192,202 @@ int analogRead(char pin) {
     // Wait for the conversion to finish
     while((ADCSRA & (1<<ADSC)) != 0);
 
+    // if (ADC) {
+      // sbi(PORTB, TRIG_INT);
+      // cbi(PORTB, TRIG_INT);
+    // } 
+
     // Return the 10 bit result
     return ADC;
 }
 
-void delay(unsigned long ms) {
-  delayMicroseconds(1000 * ms);
-}
+ISR(TIM1_COMPA_vect) {
 
-void delayMicroseconds(unsigned long us) {
-    // for the 8 MHz internal clock on the ATmega168
+  sbi(PORTB, TRIG_INT);
+  cbi(PORTB, TRIG_INT);
+  
+  // Only take the lowest 8 bits
+  // HERE
+  LightBuffer.buffer[LightBuffer.bufferLocation++] = analogRead(PHOTODIODE_PIN);
 
-    // for a one- or two-microsecond delay, simply return.  the overhead of
-    // the function calls takes more than two microseconds.  can't just
-    // subtract two, since us is unsigned; we'd overflow.
-    if (--us == 0)
-    return;
-    if (--us == 0)
-    return;
-
-    // the following loop takes half of a microsecond (4 cycles)
-    // per iteration, so execute it twice for each microsecond of
-    // delay requested.
-    us <<= 1;
-
-    // partially compensate for the time taken by the preceeding commands.
-    // we can't subtract any more than this or we'd overflow w/ small delays.
-    us--;
-
-    // busy wait
-    __asm__ __volatile__ (
-    "1: sbiw %0,1" "\n\t" // 2 cycles
-    "brne 1b" : "=w" (us) : "0" (us) // 2 cycles
-    );
-}
-
-void toggleIR(void) {
-    ((PINB & 0x01) ? cbi(PORTB, IR_TX) : sbi(PORTB, IR_TX));
-}
-
-ISR(INT0_vect, ISR_NOBLOCK){  //nested interrupts, aka stacks on stacks of interrupts.
-    USICR|=(1<<USIOIE)|(1<<USIWM0); //re-enable USI
-
-    cbi(TIMSK1, OCIE1A);
-
-    spiX_put(0x35); //ACK
-    spiX_wait();
-
-    switch(spiX_get()){
-
-    // Make sure comms are actually working
-    case 0x00:     //ACK-ACK signal
-        spiX_put(0x55);//ACK
-        spiX_wait();
-        break;
-         
-    // Read from the Mic Buffer
-     case 0x01: //READ BUF
-        for(counter=0;counter<BUF_SIZE;counter++){//read backwards through the buffer till you get to 0, then start at the buffer end
-            spiX_put(MIC_BUF[MIC_BUF_LOC]);
-            spiX_wait();
-            MIC_BUF_LOC--;
-            if(MIC_BUF_LOC<1){
-                MIC_BUF_LOC=(BUF_SIZE-1);
-            }
-        }
-        break;
-
-    // Read from the Light Buffer
-     case 0x02:
-        for(counter=0;counter<BUF_SIZE;counter++){//read backwards through the buffer till you get to 0, then start at the buffer end
-            spiX_put(LIGHT_BUF[LIGHT_BUF_LOC]);
-            spiX_wait();
-            LIGHT_BUF_LOC--;
-            if(LIGHT_BUF_LOC<1){
-                LIGHT_BUF_LOC=(BUF_SIZE-1);
-            }
-        }
-        break;
+  // If a light trigger has been set and the level is hit 
+  if (lightTrigger != 0 && LightBuffer.buffer[LightBuffer.bufferLocation - 1] >= lightTrigger) {
+    
+    // Set the read value
+    lightTriggerReadVal = LightBuffer.buffer[LightBuffer.bufferLocation - 1];
+    
+    // Raise the interrupt pin
+    sbi(PORTB, TRIG_INT);
+    
+    // Then put it back down
+    cbi(PORTB, TRIG_INT);
   }
-  // Re-enable ADC reading timer
-  sbi(TIMSK1, OCIE1A);
+  
+  if (LightBuffer.bufferLocation == BUF_SIZE) {
+    LightBuffer.bufferLocation = 0;
+  }
 
-  USICR&= ~(1<<USIOIE);  //disable USI
-  USICR&= ~(1<<USIWM0);  //disable USI
+  //Change order of ADC result in ADCL and ADCH
+  sbi(ADCSRB, ADLAR);
+  
+  // Add mic data to buffer
+  // HERE
+  analogRead(MIC_PIN);
+  
+  MicBuffer.buffer[MicBuffer.bufferLocation++] = ADCH;
+  
+
+  if (MicBuffer.bufferLocation == BUF_SIZE) {
+    MicBuffer.bufferLocation = 0;
+  }
+  
+  // HERE
+  analogRead(LOUDNESS_PIN);
+//   // Get eight upper bits of loudness
+  LoudnessBuffer.buffer[LoudnessBuffer.bufferLocation++] = ADCH;
+  
+    // If a loudness trigger has been set and the level is hit 
+  if (loudnessTrigger != 0 && LoudnessBuffer.buffer[LoudnessBuffer.bufferLocation - 1] >= loudnessTrigger) {
+    
+    // Set the read value
+    loudnessTriggerReadVal = LoudnessBuffer.buffer[LoudnessBuffer.bufferLocation - 1];
+    
+    // Raise the interrupt pin
+    sbi(PORTB, TRIG_INT);
+    
+    // Then put it back down
+    cbi(PORTB, TRIG_INT);
+  }
+
+  if (LoudnessBuffer.bufferLocation == BUF_SIZE) {
+    LoudnessBuffer.bufferLocation = 0;
+  }
+ 
+ // Reverse change of ADCL/H bit order
+  cbi(ADCSRB, ADLAR);
+}
+
+ISR(INT0_vect){
+  
+  // Disable ADC timer for now
+  cbi(TIMSK1, OCIE1A);
+  
+  // Enable interrupts (SPI needs this)
+  sei();
+
+  //re-enable USI
+  USICR|=(1<<USIOIE)|(1<<USIWM0); 
+
+  // put 'alive' bit 
+  spiX_put(ALIVE_CODE); 
+  spiX_wait();
+  
+  // Grab the command
+  char command = spiX_get();
+  
+  // Initialize variables
+  char length = 0;
+  volatile DataBuffer *dataBuffer;
+  
+   // Confirm command 
+  spiX_put(command); 
+  
+  // Wait for it to be sent
+  spiX_wait();
+  
+  // Switch based on the command
+  switch(command){ 
+  
+   // ACK command checks comms
+   case ACK_CMD:     
+    
+    //Send ACK code
+    spiX_put(ACK_CODE); 
+    
+    // Wait for it to be sent
+    spiX_wait();
+    break;
+
+  // Routine for reading buffers
+   case LIGHT_CMD: 
+   case SOUND_CMD:
+   case LOUDNESS_CMD:
+   
+   // Grab requested buffer
+   dataBuffer = Buffers[command - 1]; 
+   // Grab read length
+   length = spiX_get(); 
+   
+   // Echo read length
+   spiX_put(length); 
+   // Wait for echo to complete
+   spiX_wait();
+   
+   // Iterate through buffer
+   for(counter=0;counter<length;counter++){ 
+     
+     // Put the byte at the current location in the buffer
+     spiX_put(dataBuffer->buffer[dataBuffer->bufferLocation]);
+     
+     // Wait for it to be sent
+     spiX_wait();
+     
+     // Decrement buffer (we read going backwards)
+     dataBuffer->bufferLocation--;
+     
+     // If the buffer is at 0
+     if (dataBuffer->bufferLocation < 0) {
+       
+       // Set it to the end of the buffer
+       dataBuffer->bufferLocation = (BUF_SIZE-1); 
+     }
+    }
+    
+    // Put the stop command
+    spiX_put(STOP_CMD);
+    
+    // Wait for it to be sent
+    spiX_wait();
+    break;
+    
+   case LIGHT_TRIGGER_CMD:
+     lightTrigger = spiX_get();
+     spiX_put(lightTrigger);
+     spiX_wait();
+     break;
+     
+   case LOUDNESS_TRIGGER_CMD:
+     loudnessTrigger = spiX_get();
+     spiX_put(loudnessTrigger);
+     spiX_wait();
+     break;
+     
+   case TRIGGER_FETCH_CMD:
+     // Put Light Trigger Val
+     spiX_put(lightTriggerReadVal);
+     
+     // Wait for it to go through
+     spiX_wait();
+     
+     // Clear it
+     lightTriggerReadVal = 0;
+     
+     // Put Loudnesss Trigger Val
+     spiX_put(loudnessTriggerReadVal);
+    
+      // Wait for it to be sent
+     spiX_wait(); 
+     
+     // Clear it
+     loudnessTriggerReadVal = 0;
+     
+     break;
+ }
+  
+  // Disable USI
+  USICR&= ~(1<<USIOIE);
+  USICR&= ~(1<<USIWM0);
+  
+  // Re-enable ADC reads
+  sbi(TIMSK1, OCIE1A);
 }
