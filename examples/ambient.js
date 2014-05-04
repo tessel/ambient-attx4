c0a0031ad75fd7adf90f8cbae885056ad317b5dc
Frijol <ifoundthemeaningoflife@gmail.com>
2014-05-03 22:14:54 -0700

    renamed ambient example to fit convention and added descriptive text to example head

--- examples/ambient.js
+++ examples/ambient.js
@@ -1,3 +1,10 @@
+/*********************************************
+This ambient module example reports sound and
+light levels to the console, and console.logs
+whenever a specified light or sound level
+trigger is met.
+*********************************************/
+
 var tessel = require('tessel');
 var ambientPort = tessel.port('a');
 
@@ -17,7 +24,6 @@ require('../').use(ambientPort, function(err, ambient) {
 
   // Get a stream of sound level data
   ambient.on('sound', function(data) {
-
     console.log("Got some  sound: ", data);
   });
 
@@ -26,8 +32,8 @@ require('../').use(ambientPort, function(err, ambient) {
   ambient.setLightTrigger(0.15);
 
   ambient.on('light-trigger', function(data) {
-    console.log("Our light trigger was hit:", data); 
-    
+    console.log("Our light trigger was hit:", data);
+
     // Clear the trigger so it stops firing
     ambient.clearLightTrigger();
   });
@@ -39,9 +45,9 @@ require('../').use(ambientPort, function(err, ambient) {
   ambient.setSoundTrigger(0.001);
 
   ambient.on('sound-trigger', function(data) {
-    
+
     console.log("Something happened with sound: ", data);
-    
+
     // Clear it
     ambient.clearSoundTrigger();
   });
