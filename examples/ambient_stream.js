92fbce2182559e22a0ac4d6328359ad74466ad9f
pmcote <pmcote1@gmail.com>
2014-05-27 17:24:05 -0700

    fixed example to style guide specs. I have learned the error of my ways.

--- examples/ambient_stream.js
+++ examples/ambient_stream.js
@@ -12,16 +12,16 @@ var ambient = require('../').use(tessel.port('A')); // Replace '../' with 'ambie
 ambient.on('ready', function () {
  // Two ways to get the stream of data coming in. . .
 
- 	setInterval(function () {
-	 	ambient.getLightBuffer(function(err, data) {
-	 		console.log('Light data, buffer method: ' + data);
-	 	});
- 	}, 2000); //Set interval to whatever you want to get the 10 most recent readings.
+  setInterval(function () {
+	  ambient.getLightBuffer(function(err, data) {
+	 	  console.log('Light data, buffer method: ' + data);
+	  });
+  }, 2000); //Set interval to whatever you want to get the 10 most recent readings.
 
 
-	ambient.on('light', function (lightData) {
-		console.log('Light data, stream method: ' + lightData);
-	});
+  ambient.on('light', function (lightData) {
+	  console.log('Light data, stream method: ' + lightData);
+  });
   
 });
 
