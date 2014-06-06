6241b1a08b97768502868262e4c6e99d0f524a83
pmcote <pmcote1@gmail.com>
2014-06-06 14:10:29 -0700

    polishing the readme and examples

--- examples/ambient_stream.js
+++ examples/ambient_stream.js
@@ -7,7 +7,9 @@ getting the ambient module's data.
 *********************************************/
 
 var tessel = require('tessel');
-var ambient = require('../').use(tessel.port['A']); // Replace '../' with 'ambient-attx4' in your own code
+var ambientlib = require('../');// Replace '../' with 'ambient-attx4' in your own code
+
+var ambient = ambientlib.use(tessel.port['A']); 
 
 ambient.on('ready', function () {
 
