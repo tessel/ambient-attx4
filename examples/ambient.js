d20479ac7d96dc7d6d1739609243c6518fedd837
pmcote <pmcote1@gmail.com>
2014-06-11 10:23:56 -0700

    adds light and sound readout to go with trigger alerts

--- examples/ambient.js
+++ examples/ambient.js
@@ -2,17 +2,25 @@
 // http://creativecommons.org/publicdomain/zero/1.0/
 
 /*********************************************
-This ambient module example console.logs
-whenever a specified light or sound level
-trigger is met.
+This ambient module example console.logs 
+ambient light and sound levels and whenever a 
+specified light or sound level trigger is met.
 *********************************************/
 
 var tessel = require('tessel');
 var ambientlib = require('../');// Replace '../' with 'ambient-attx4' in your own code
 
-var ambient = ambientlib.use(tessel.port['A']); 
+var ambient = ambientlib.use(tessel.port['A']);
 
 ambient.on('ready', function () {
+ // Get points of light and sound data.
+  setInterval( function () {
+    ambient.getLightLevel( function(err, ldata) {
+      ambient.getLightLevel( function(err, sdata) {
+        console.log("Light level: ", ldata, "Sound Level: ", sdata);
+    });
+  })}, 3000); // The readings will happen every three seconds unless the trigger is hit
+
   ambient.setLightTrigger(0.5);
 
   // Set a light level trigger
