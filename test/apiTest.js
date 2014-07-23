6d67c18f7ee58ed626424c426c4ff5695c09956a
Frijol <ifoundthemeaningoflife@gmail.com>
2014-07-23 09:59:25 -0700

    tests written properly, need to be checked with Tessel plugged in

--- test/apiTest.js
+++ test/apiTest.js
@@ -1,174 +1,302 @@
-var assert = require('assert');
+var test = require('tinytap');
 
+var async = require('async');
+
+var portname = process.argv[2] || 'A';
 var tessel = require('tessel');
-var startTime = new Date(milliseconds);
-var ambient = require('../').use(tessel.port[process.argv[2] || 'A']);
-var requireTime = new Date(milliseconds);
+var ambientLib = require('../');
+var ambient;
 
-// Make sure ambient-attx4 requires in a reasonable amount of time
-assert(requireTime - startTime < 1500, 'timed out requiring ambient-attx4');
-
-
-//***Events***//
-//ready
-// It calls ready within a reasonable amount of time
-ambient.on('ready', function() {
-  var readyTime = new Date(milliseconds);
-  assert(readyTime - requireTime < 500, 'timed out waiting for ready event');
-//light
-  // It gets light data within a reasonable amount of time
-  var firstData = true;
-  ambient.on('light', function(data) {
-    if(firstData) {
-      var dataTime = new Date(milliseconds);
-      assert(dataTime - readyTime < 300, 'timed out waiting for initial light data');
-      // Check the data to make sure it's valid
-      checkValidData(data);
-      firstData = false;
-      ambient.removeAllListeners('light');
-    }
-  });
-});
-//sound
-  // It gets sound data within a reasonable amount of time
-  var firstData = true;
-  ambient.on('sound', function(data) {
-    if(firstData) {
-      var dataTime = new Date(milliseconds);
-      assert(dataTime - readyTime < 300, 'timed out waiting for initial sound data');
-      // Check the data to make sure it's valid
-      checkValidData(data);
-      firstData = false;
-      ambient.removeAllListeners('sound');
-    }
-  });
-});
-
-//error
-// Fail if we get an error
-ambient.on('error', function (err) {
-  assert(false, 'error caught: ' + err);
-});
+async.series([
+  // Test connecting
+  test('Connecting to ambient module', function (t) {
+    ambient = ambientLib.use(tessel.port[portname], function (err, ambient) {
+      t.ok(ambient, 'The ambient module object was not returned');
+      t.equal(err, undefined, 'There was an error connecting');
+      // Test events
+      var timeout = 1000;
+      // ready
+      var readyTimer = setTimeout(function () {
+        t.ok(false, 'failed to emit ready event in a reasonable amount of time');
+        t.end();
+      }, timeout);
+      ambient.on('ready', function () {
+        clearTimeout(readyTimer);
+        t.ok(true, 'ready was emitted');
+        // check data
+        var numEvents = 2;
+        var countEvents = 0;
+        // light data
+        var lightDataTimer = setTimeout(function () {
+          t.ok(false, 'failed to emit a light data event in a reasonable amount of time');
+          t.end();
+        }, timeout);
+        ambient.on('light', function (data) {
+          clearTimeout(lightDataTimer);
+          t.ok(true, 'light data was emitted');
+          // Check valid data
+          t.ok(data.length > 0, 'the data reading has no length');
+          // For each data point:
+          data.forEach(function (datum,index) {
+            // It's a number...
+            t.equal(typeof datum, 'number', 'value ' + datum + ' should be a number');
+            // ...between 0 and 1
+            t.ok(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
+            if (index == (data.length - 1)) {
+              // clean up
+              ambient.removeAllListeners('light');
+              countEvents++;
+              if(countEvents == numEvents) {
+                t.end();
+              }
+            }
+          });
+        });
+        // sound data
+        var soundDataTimer = setTimeout(function () {
+          t.ok(false, 'failed to emit a sound data event in a reasonable amount of time');
+          t.end();
+        }, timeout);
+        ambient.on('sound', function (data) {
+          clearTimeout(soundDataTimer);
+          t.ok(true, 'sound data was emitted');
+          // Check valid data
+          t.ok(data.length > 0, 'the data reading has no length');
+          // For each data point:
+          data.forEach(function (datum,index) {
+            // It's a number...
+            t.equal(typeof datum, 'number', 'value ' + datum + ' should be a number');
+            // ...between 0 and 1
+            t.ok(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
+            if (index == (data.length - 1)) {
+              // clean up
+              ambient.removeAllListeners('sound');
+              countEvents++;
+              if(countEvents == numEvents) {
+                t.end();
+              }
+            }
+          });
+        });
+      });
+      // error
+      // Fail if we get an error
+      ambient.on('error', function (err) {
+        t.ok(false, 'error caught: ' + err);
+        t.end();
+      });
+    });
+  }),
   
-//***Triggers***//
-// Trigger value set very low to trip in ambient conditions
-var triggerVal = 0.001;
-// "Reasonable time" to expect event emission
-var timeout = 800;
-
-// Light trigger tests
-// Test setting of light trigger
-ambient.setLightTrigger(triggerVal, function (err, val) {
-  if(err) {
-    assert(false, 'error caught: ' + err);
-  }
-  assert(triggerVal == val, 'light trigger set to ' + val + ' when it should have been set to ' + triggerVal);
-  // Fail if light trigger not hit in a reasonable time
-  var failLightTrigger = setTimeout(function () {
-    assert(false, 'light trigger timed out- trigger improperly set or light-trigger not emitting');
-  }, timeout);
-  // If light trigger emitted
-  ambient.on('light-trigger', function (val) {
-    // Timeout cancelled
-    clearTimeout(failLightTrigger);
-    // Check to ensure valid data
-    checkValidDataPoint(val);
-    // Test clearing of light trigger
-    ambient.clearLightTrigger(function (err, val) {
+  // Test triggers
+  // light trigger functions and events
+  test('light trigger tests', function (t) {
+    var triggerVal = 0.001; // Trigger value set very low to trip in ambient conditions
+    var timeout = 800; // "Reasonable time" to expect event emission
+    var tolerance = triggerVal; // Tolerance on returned value for set trigger
+    // Test setting of light trigger
+    ambient.setLightTrigger(triggerVal, function (err, val) {
       if(err) {
-        assert(false, 'error caught: ' + err);
+        t.ok(false, 'error caught: ' + err);
       }
-      // Fail if we get any more light triggers
-      ambient.on('light-trigger', function (val) {
-        assert(false, 'light-trigger event emitted even after trigger cleared- trigger improperly cleared or bad event emission');
-      });
-      setTimeout(function () {
-        // Stop listening; it passed
-        ambient.removeAllListeners('light-trigger');
+      t.ok(val < triggerVal + tolerance && val > triggerVal - tolerance, 'light trigger set to ' + val + ' when it should have been set to ' + triggerVal);
+      // Fail if light trigger not hit in a reasonable time
+      var failLightTrigger = setTimeout(function () {
+        t.ok(false, 'light trigger timed out- trigger improperly set or light-trigger not emitting. Or things are very dark.');
       }, timeout);
+      // If light trigger emitted
+      ambient.on('light-trigger', function (datum) {
+        // Timeout cancelled
+        clearTimeout(failLightTrigger);
+        // Track progress of tests happening asynchronously
+        var numEvents = 2;
+        var countEvents = 0;
+        // Check valid data
+        // It's a number...
+        t.equal(typeof datum, 'number', 'value ' + datum + ' should be a number');
+        // ...between 0 and 1...
+        t.ok(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
+        // ...and it's higher than the trigger value
+        t.ok(datum > triggerVal, 'trigger tripped on an incorrect data value');
+        countEvents++;
+        if(countEvents == numEvents) {
+          t.end();
+        }
+        // Test clearing of light trigger
+        ambient.clearLightTrigger(function (err, val) {
+          if(err) {
+            t.ok(false, 'error caught: ' + err);
+          }
+          // Fail if we get any more light triggers
+          ambient.on('light-trigger', function (val) {
+            t.ok(false, 'light-trigger event emitted even after trigger cleared- trigger improperly cleared or bad event emission');
+          });
+          var checkClearLightTrigger = setTimeout(function () {
+            // Stop listening; it passed the clearLightTrigger test
+            ambient.removeAllListeners('light-trigger');
+            // Make sure it doesn't fire when the trigger is set way higher than ambient
+            ambient.setLightTrigger(0.9, function (err, val) {
+              if(err) {
+                t.ok(false, 'error caught: ' + err);
+              }
+              // Fail if we get a light trigger
+              ambient.on('light-trigger', function (val) {
+                t.ok(false, 'light-trigger event emitted with really high trigger value. Bad event emission or ridiculously bright test location');
+              });
+              var checkHighLightTrigger = setTimeout(function () {
+                // Stop listening; it passed the super high light trigger test
+                ambient.removeAllListeners('light-trigger');
+                countEvents++;
+                if(countEvents == numEvents) {
+                  t.end();
+                }
+              }, timeout);
+            });
+          }, timeout);
+        });
+      });
     });
-  });
-});
-
-// Sound trigger tests
-// Test setting of sound trigger
-ambient.setSoundTrigger(triggerVal, function (err, val) {
-  if(err) {
-    assert(false, 'error caught: ' + err);
-  }
-  assert(triggerVal == val, 'sound trigger set to ' + val + ' when it should have been set to ' + triggerVal);
-  // Fail if sound trigger not hit in a reasonable time
-  var failSoundTrigger = setTimeout(function () {
-    assert(false, 'sound trigger timed out- trigger improperly set or sound-trigger not emitting');
-  }, timeout);
-  // If sound trigger emitted
-  ambient.on('sound-trigger', function (val) {
-    // Timeout cancelled
-    clearTimeout(failSoundTrigger);
-    // Check to ensure valid data
-    checkValidDataPoint(val);
-    // Test clearing of sound trigger
-    ambient.clearSoundTrigger(function (err, val) {
+  }),
+  
+  // sound trigger functions and events
+  test('sound trigger tests', function (t) {
+    var triggerVal = 0.001; // Trigger value set very low to trip in ambient conditions
+    var timeout = 800; // "Reasonable time" to expect event emission
+    var tolerance = triggerVal; // Tolerance on returned value for set trigger
+    // Test setting of sound trigger
+    ambient.setSoundTrigger(triggerVal, function (err, val) {
       if(err) {
-        assert(false, 'error caught: ' + err);
+        t.ok(false, 'error caught: ' + err);
       }
-      // Fail if we get any more sound triggers
-      ambient.on('sound-trigger', function (val) {
-        assert(false, 'sound-trigger event emitted even after trigger cleared- trigger improperly cleared or bad event emission');
-      });
-      setTimeout(function () {
-        // Stop listening; it passed
-        ambient.removeAllListeners('sound-trigger');
+      t.ok(val < triggerVal + tolerance && val > triggerVal - tolerance, 'sound trigger set to ' + val + ' when it should have been set to ' + triggerVal);
+      // Fail if light trigger not hit in a reasonable time
+      var failSoundTrigger = setTimeout(function () {
+        t.ok(false, 'sound trigger timed out- trigger improperly set or sound-trigger not emitting. Or things are very quiet.');
       }, timeout);
+      // If sound trigger emitted
+      ambient.on('sound-trigger', function (datum) {
+        // Timeout cancelled
+        clearTimeout(failSoundTrigger);
+        // Track progress of tests happening asynchronously
+        var numEvents = 2;
+        var countEvents = 0;
+        // Check valid data
+        // It's a number...
+        t.equal(typeof datum, 'number', 'value ' + datum + ' should be a number');
+        // ...between 0 and 1...
+        t.ok(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
+        // ...and it's higher than the trigger value
+        t.ok(datum > triggerVal, 'trigger tripped on an incorrect data value');
+        countEvents++;
+        if(countEvents == numEvents) {
+          t.end();
+        }
+        // Test clearing of sound trigger
+        ambient.clearSoundTrigger(function (err, val) {
+          if(err) {
+            t.ok(false, 'error caught: ' + err);
+          }
+          // Fail if we get any more sound triggers
+          ambient.on('sound-trigger', function (val) {
+            t.ok(false, 'sound-trigger event emitted even after trigger cleared- trigger improperly cleared or bad event emission');
+          });
+          var checkClearSoundTrigger = setTimeout(function () {
+            // Stop listening; it passed the clearSoundTrigger test
+            ambient.removeAllListeners('sound-trigger');
+            // Make sure it doesn't fire when the trigger is set way higher than ambient
+            ambient.setSoundTrigger(0.9, function (err, val) {
+              if(err) {
+                t.ok(false, 'error caught: ' + err);
+              }
+              // Fail if we get a sound trigger
+              ambient.on('sound-trigger', function (val) {
+                t.ok(false, 'sound-trigger event emitted with really high trigger value. Bad event emission or ridiculously loud test location');
+              });
+              var checkHighSoundTrigger = setTimeout(function () {
+                // Stop listening; it passed the super high sound trigger test
+                ambient.removeAllListeners('sound-trigger');
+                countEvents++;
+                if(countEvents == numEvents) {
+                  t.end();
+                }
+              }, timeout);
+            });
+          }, timeout);
+        });
+      });
     });
-  });
-});
+  }),
+  
+  // tested to here
+  
+  // Methods
+  test('getLightBuffer', function (t) {
+    ambient.getLightBuffer(function (err, data) {
+      if(err) {
+        t.ok(false, 'error caught: ' + err);
+      }
+      // Check valid data
+      t.ok(data.length > 0, 'the data reading has no length');
+      // For each data point:
+      data.forEach(function (datum,index) {
+        // It's a number...
+        t.equal(typeof datum, 'number', 'value ' + datum + ' should be a number');
+        // ...between 0 and 1
+        t.ok(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
+        if (index == (data.length - 1)) {
+          t.end();
+        }
+      });
+    });
+  }),
+  
+  test('getLightLevel'), function (t) {
+    ambient.getLightLevel(function (err, datum) {
+      if(err) {
+        t.ok(false, 'error caught: ' + err);
+      }
+      // Check valid data
+      // It's a number...
+      t.equal(typeof datum, 'number', 'value ' + datum + ' should be a number');
+      // ...between 0 and 1
+      t.ok(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
+      t.end();
+    });
+  }),
+  
+  test('getSoundBuffer', function (t) {
+    ambient.getSoundBuffer(function (err, data) {
+      if(err) {
+        t.ok(false, 'error caught: ' + err);
+      }
+      // Check valid data
+      t.ok(data.length > 0, 'the data reading has no length');
+      // For each data point:
+      data.forEach(function (datum,index) {
+        // It's a number...
+        t.equal(typeof datum, 'number', 'value ' + datum + ' should be a number');
+        // ...between 0 and 1
+        t.ok(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
+        if (index == (data.length - 1)) {
+          t.end();
+        }
+      });
+    });
+  }),
 
-//***Methods***//
-//getLightBuffer
-ambient.getLightBuffer(function (err, data) {
-  if(err) {
-    assert(false, 'error caught: ' + err);
-  }
-  checkValidData(data);
-});
+  
+  test('getSoundLevel', function (t) {
+    ambient.getSoundLevel(function (err, datum) {
+      if(err) {
+        t.ok(false, 'error caught: ' + err);
+      }
+      // Check valid data
+      // It's a number...
+      t.equal(typeof datum, 'number', 'value ' + datum + ' should be a number');
+      // ...between 0 and 1
+      t.ok(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
+      t.end();
+    });
+  })
 
-//getLightLevel
-ambient.getLightLevel(function (err, data) {
-  if(err) {
-    assert(false, 'error caught: ' + err);
-  }
-  checkValidDataPoint(data);
-});
-
-//getSoundBuffer
-ambient.getSoundBuffer(function (err, data) {
-  if(err) {
-    assert(false, 'error caught: ' + err);
-  }
-  checkValidData(data);
-});
-
-//getSoundLevel
-ambient.getSoundLevel(function (err, data) {
-  if(err) {
-    assert(false, 'error caught: ' + err);
-  }
-  checkValidDataPoint(data);
-});
-
-function checkValidData(dataArray) {
-  // Data has length
-  assert(dataArray.length > 0, 'the data reading has no length');
-  // The things in data are...
-  dataArray.forEach(function (val) {
-    checkValidDataPoint(val);
-  });
-}
-
-function checkValidDataPoint(datum) {
-  // It's a number...
-  assert((typeof datum) == 'number', 'value ' + datum + ' should be a number');
-  // ...between 0 and 1
-  assert(datum >= 0.0 && datum <= 1.0, 'data point ' + datum + ' is out of range');
-}
+  ]);
