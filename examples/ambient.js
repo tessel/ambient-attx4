6853e2670bcd8ed1d770554766790f453b532670
Kelsey <ifoundthemeaningoflife@gmail.com>
2016-03-12 10:19:56 +0200

    Update ambient.js

--- examples/ambient.js
+++ examples/ambient.js
@@ -15,15 +15,16 @@ var ambient = ambientlib.use(tessel.port['A']);
 ambient.on('ready', function () {
  // Get points of light and sound data.
   setInterval( function () {
-    ambient.getLightLevel( function(err, ldata) {
+    ambient.getLightLevel( function(err, lightdata) {
       if (err) throw err;
-      ambient.getSoundLevel( function(err, sdata) {
+      ambient.getSoundLevel( function(err, sounddata) {
         if (err) throw err;
-        console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
+        console.log("Light level:", lightdata.toFixed(8), " ", "Sound Level:", sounddata.toFixed(8));
+      });
     });
-  })}, 500); // The readings will happen every .5 seconds unless the trigger is hit
+  }, 500); // The readings will happen every .5 seconds
 });
 
 ambient.on('error', function (err) {
-  console.log(err)
+  console.log(err);
 });
