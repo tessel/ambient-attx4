87b34b689830f87e42a7b9dbbf77e029982ffb7c
johnnyman727 <johnnyman727@gmail.com>
2016-03-12 09:45:33 +0200

    Splits up main example into two discrete use cases

---
+++ examples/ambient-triggers.js
@@ -0,0 +1,56 @@
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+/*********************************************
+This ambient module example console.logs
+ambient light and sound levels and whenever a
+specified light or sound level trigger is met.
+*********************************************/
+
+var tessel = require('tessel');
+var ambientlib = require('../'); // Replace '../' with 'ambient-attx4' in your own code
+
+var ambient = ambientlib.use(tessel.port['A']);
+
+ambient.on('ready', function () {
+
+  ambient.setLightTrigger(0.5);
+
+  // Set a light level trigger
+  // The trigger is a float between 0 and 1
+  ambient.on('light-trigger', function(data) {
+    console.log("Our light trigger was hit:", data);
+
+    // Clear the trigger so it stops firing
+    ambient.clearLightTrigger();
+    //After 1.5 seconds reset light trigger
+    setTimeout(function () {
+
+        ambient.setLightTrigger(0.5);
+
+    },1500);
+  });
+
+  // Set a sound level trigger
+  // The trigger is a float between 0 and 1
+  ambient.setSoundTrigger(0.1);
+
+  ambient.on('sound-trigger', function(data) {
+    console.log("Something happened with sound: ", data);
+
+    // Clear it
+    ambient.clearSoundTrigger();
+
+    //After 1.5 seconds reset sound trigger
+    setTimeout(function () {
+
+        ambient.setSoundTrigger(0.1);
+
+    },1500);
+
+  });
+});
+
+ambient.on('error', function (err) {
+  console.log(err)
+});
