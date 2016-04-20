96c42d313187d6ac2ba479a5fb60e14494d4c477
Rick Waldron <waldron.rick@gmail.com>
2016-04-20 10:58:51 -0400

    Integrate project tooling
    This toolset is identical to the toolset used on t2-cli, t2-firmware & tessel-av (t2-compiler has the same, but the servr isn't written yet, so it's not in use).
    
    Currently, almost nothing passes.
    
    - grunt jshint: fail; all files need to be cleaned up to pass
    - grunt jscs: fail; all files need to be cleaned up to pass
    - grunt nodeunit: pass; there is only one test, @wyze will be implementing more
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

---
+++ test/common/bootstrap.js
@@ -0,0 +1,24 @@
+global.IS_TEST_ENV = true;
+
+// Dependencies: Built-in
+global.cp = require('child_process');
+global.events = require('events');
+global.fs = require('fs');
+global.path = require('path');
+global.stream = require('stream');
+global.util = require('util');
+
+// Dependencies: Third Party
+global.sinon = require('sinon');
+
+// Dependencies: Internal
+global.Attiny = require('./attiny-common-mock');
+global.exported = require('../../index');
+
+
+global.EventEmitter = events.EventEmitter;
+global.sandbox = global.sinon.sandbox.create();
+global.Ambient = exported.Ambient;
+
+// Notes:
+// - `exported` will likely be usefule for stubbing `use`
