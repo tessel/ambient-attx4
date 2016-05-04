b2f0796a3f3da307c0d4a06b008c558dce854243
Neil Kistner <neil.kistner@gmail.com>
2016-05-03 19:12:37 -0500

    Add test for initialization (#66)

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,6 +1,5 @@
 exports['Ambient'] = {
   setUp: function(done) {
-    // Make stubs/spies/mocks with `sandbox`
     done();
   },
 
@@ -9,10 +8,46 @@ exports['Ambient'] = {
     done();
   },
 
-  isAFunction: function(test) {
-    test.expect(1);
+  initialization: function(test) {
+    test.expect(10);
 
-    test.equal(typeof Ambient, 'function');
+    this.attinyInitialized = sandbox.stub(Attiny.prototype, 'initialize');
+    this.sensor = new Ambient();
+
+    test.equal(typeof this.sensor.attiny, 'object');
+    test.equal(this.sensor.connected, false);
+    test.equal(this.sensor.lightTriggerLevel, null);
+    test.equal(this.sensor.soundTriggerLevel, null);
+    test.equal(this.sensor.pollingFrequency, 500);
+    test.equal(this.sensor.lightPolling, false);
+    test.equal(this.sensor.soundPolling, false);
+    test.equal(this.sensor.pollInterval, undefined);
+    test.equal(this.attinyInitialized.callCount, 1);
+
+    // called with firmwareOptions, callback
+    test.equal(this.attinyInitialized.args[0].length, 2);
     test.done();
+  },
+
+  attinyInitialization: function(test) {
+    test.expect(3);
+
+    this.attinyInitialized = sandbox.stub(Attiny.prototype, 'initialize', (_, callback) => {
+      callback(null, this.sensor);
+    });
+
+    this.setIRQCallback = sandbox.stub(Attiny.prototype, 'setIRQCallback');
+
+    this.sensor = new Ambient();
+    this.sensor.on('ready', () => {
+      // ready event is in a setImmediate call
+      // setIRQCallback can't be tested without this being a setImmediate block
+      setImmediate(() => {
+        test.equal(this.sensor.connected, true);
+        test.equal(this.setIRQCallback.callCount, 1);
+        test.equal(this.setIRQCallback.calledWith(this.sensor.irqwatcher), true);
+        test.done();
+      });
+    });
   }
 };
